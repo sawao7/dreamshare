3f683a6f2fd9348f2a2a73349c5856d447b2f281
澤田悠太 <yuta2929ayax@gmail.com>
2022-05-13 17:11:13 +0900

    finish function of google ogin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,38 @@
 import React from "react";
+import firebase from "./firebase";
+
+import { Link, useNavigate } from "react-router-dom";
 
 export const Header = (props) => {
 	const user = props.user;
+	const navigate = useNavigate();
+
+	// google login function
+	const GoogleLogin = () => {
+		const provider = new firebase.auth.GoogleAuthProvider();
+		firebase.auth().signInWithRedirect(provider);
+	};
+
+	// google logout function
+	const GoogleLogout = () => {
+		firebase.auth().signOut();
+		navigate("/");
+	};
 	return (
 		<div>
-			<div>{user ? <div>ログイン済み</div> : <div>ログインして</div>}</div>
+			<div>
+				{user ? (
+					<div>
+						<div>ログイン済み</div>
+						<button onClick={() => GoogleLogout()}>Logout</button>
+					</div>
+				) : (
+					<div>
+						<div>ログインして</div>
+						<button onClick={() => GoogleLogin()}>Login</button>
+					</div>
+				)}
+			</div>
 		</div>
 	);
 };
