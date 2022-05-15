f89cc36357a6caad0eea5d6b505710a41db71482
澤田悠太 <yuta2929ayax@gmail.com>
2022-05-15 11:02:30 +0900

    change home for getting API

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import axios from "axios";
 
 export const Home = (props) => {
 	const user = props.user;
+	const [text, setText] = React.useState();
+	const url = "https://b4im3j.deta.dev/";
+
+	React.useEffect(() => {
+		axios.get(url).then((response) => {
+			setText(response.data);
+		});
+	}, []);
 
 	return (
-		<div>
+		<div className="text-white">
 			<div>home</div>
+			{text ? text.Hello : "なし"}
 		</div>
 	);
 };
