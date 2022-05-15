cfe326f24eec549f7833e30659dca4b3b45bfd55
澤田悠太 <yuta2929ayax@gmail.com>
2022-05-15 14:00:08 +0900

    add api reference

--- src/components/Create/Create.jsx
+++ src/components/Create/Create.jsx
@@ -1,11 +1,39 @@
+import axios from "axios";
 import React from "react";
 
+// 文字の送信、画像の表示
 export const Create = (props) => {
 	const user = props.user;
+	const [text, setText] = React.useState();
+	const [image, setImage] = React.useState();
+
+	const GetInputText = (e) => {
+		setText(e.target.value);
+	};
+
+	const SubmitText = () => {
+		console.log(text);
+		setText("");
+		axios
+			.get("https://logos-download.com/wp-content/uploads/2016/09/React_logo_wordmark.png", {
+				responseType: "blob",
+			})
+			.then((res) => {
+				setImage([URL.createObjectURL(res.data)]);
+			});
+	};
 
 	return (
-		<div>
+		<div className="p-10 text-white h-96">
 			<div>Create</div>
+			<div>
+				<input onChange={GetInputText} value={text} className="text-black"></input>
+				<button onClick={SubmitText} className="border border-cyan-400">
+					Create
+				</button>
+			</div>
+			{/* 画像表示 */}
+			{image ? <img src={image} alt="text" className="" /> : "text"}
 		</div>
 	);
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,18 +4,34 @@ import axios from "axios";
 export const Home = (props) => {
 	const user = props.user;
 	const [text, setText] = React.useState();
-	const url = "https://b4im3j.deta.dev/";
+	const [image, setImage] = React.useState();
+
+	const url = "https://fastapi-for-test.herokuapp.com/file";
+	// const url = "http://127.0.0.1:8000/file";
 
 	React.useEffect(() => {
-		axios.get(url).then((response) => {
-			setText(response.data);
-		});
+		// axios.get(url).then((response) => {
+		// 	setText(response.data);
+		// 	console.log(response);
+		// });
+		// axios.get(url).then((res) => {
+		// 	setImage(res.data);
+		// 	console.log(res.data);
+		// });
+		axios
+			.get(url, {
+				responseType: "blob",
+			})
+			.then((res) => {
+				setImage([URL.createObjectURL(res.data)]);
+			});
 	}, []);
 
 	return (
 		<div className="text-white">
 			<div>home</div>
-			{text ? text.Hello : "なし"}
+			{/* {text ? text.Hello : "なし"} */}
+			{image ? <img src={image} alt="text" className="" /> : "text"}
 		</div>
 	);
 };
